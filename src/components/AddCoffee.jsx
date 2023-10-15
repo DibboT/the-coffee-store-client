ad9db1de2af1654206fb92750098bc5c75118531
Dibbo T <dibbotripura7@gmail.com>
2023-10-15 13:03:54 +0600

    data is sending to sever

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -3,6 +3,32 @@
 const AddCoffee = () => {
     const handleAddCoffee = event => {
         event.preventDefault();
+
+        const form = event.target;
+
+        const name = form.name.value;
+        const quantity = form.quantity.value;
+        const supplier = form.supplier.value;
+        const taste = form.taste.value;
+        const category = form.category.value;
+        const details = form.details.value;
+        const photo = form.photo.value;
+
+        const newCoffee = {name, quantity, supplier, taste, category, details, photo}
+        console.log(newCoffee);
+
+        fetch('http://localhost:5000/coffee', {
+            method: "POST",
+            headers: {
+                'content-type' : 'application/json'
+            },
+            body: JSON.stringify(newCoffee)
+        })
+
+        .then(res=> res.json())
+        .then(data => {
+            console.log(data);
+        })
     }
     return (
         <div className="bg-[#F4F3F0] p-24">
