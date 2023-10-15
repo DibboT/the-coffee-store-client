c71ce4d1fe954d5b7c10897f14ea7fe794aae4d6
Dibbo T <dibbotripura7@gmail.com>
2023-10-15 14:37:45 +0600

    sweet alert has been installed

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,4 +1,4 @@
-
+import Swal from 'sweetalert2'
 
 const AddCoffee = () => {
     const handleAddCoffee = event => {
@@ -28,10 +28,19 @@ const AddCoffee = () => {
         .then(res=> res.json())
         .then(data => {
             console.log(data);
+            if(data.insertedId){
+                Swal.fire({
+                    title: 'Sucess',
+                    text: 'User Added Sucessfully',
+                    icon: 'sucess',
+                    confirmButtonText: 'Cool'
+                  })
+            }
         })
     }
     return (
-        <div className="bg-[#F4F3F0] p-24">
+      
+        <div className="bg-[#F4F3F0] p-10 w-1/2 mx-auto ">
             <h2 className="text-center mb-4 text-3xl font-bold">Add Coffee</h2>
             <form onSubmit={handleAddCoffee}>
                 {/*Form Row: Coffee Name And Quantity */}
